e2895fc1243afadfc78b8b900392d3a419c6d1ef
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-05 18:06:25 +0100

    Install Swapy and test it

---
+++ src/components/Swapy.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { createSwapy } from 'swapy'
+	import { onMount } from 'svelte'
+
+	let swapy
+
+	onMount(() => {
+		const container = document.querySelector('.container')
+
+		if (container) {
+			swapy = createSwapy(container, {
+				animation: 'spring'
+			})
+			swapy.enable(true)
+		}
+	})
+</script>
+
+<div class="container flex flex-col gap-y-4 p-4">
+	<div class="section-1" data-swapy-slot="foo">
+		<div class="border-2 border-[#fff] p-4" data-swapy-item="a">
+			<p>FIRST ELEMENT</p>
+		</div>
+	</div>
+
+	<div class="section-2" data-swapy-slot="bar">
+		<div class="border-2 border-[#fff] p-4" data-swapy-item="b">
+			<p>SECOND ELEMENT</p>
+		</div>
+	</div>
+
+	<div class="section-3" data-swapy-slot="baz">
+		<div class="border-2 border-[#fff] p-4" data-swapy-item="c">
+			<p>THIRD ELEMENT</p>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
+<script>
+	import Swapy from '../components/Swapy.svelte'
+</script>
+
 <header>
 	<h1>DevSvg</h1>
 </header>
 
-<main class="border-y-4 border-y-[#9E66FF]/40 bg-[#2A2D2E]">
+<main class="grid grid-cols-[0.3fr_auto_0.2fr] border-y-4 border-y-[#9E66FF]/40 bg-[#2A2D2E] p-4">
+	<Swapy />
+	<p>Welcome to SvelteKit</p>
 	<p>Welcome to SvelteKit</p>
 </main>
 
