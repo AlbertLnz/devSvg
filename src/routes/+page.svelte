68a15a951528b41d6bc36667b7c21847173b5534
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-05 17:50:27 +0100

    app layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header>
+	<h1>DevSvg</h1>
+</header>
+
+<main class="border-y-4 border-y-[#9E66FF]/40 bg-[#2A2D2E]">
+	<p>Welcome to SvelteKit</p>
+</main>
+
+<footer>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</footer>
