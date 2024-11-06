35584cc32a421f8de5d0000b7bbd54cd5d8aae6d
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-06 10:17:18 +0100

    Add 2 components (SvgProduct & SvgConfiguration). SvgProduct showing the final result & SvgConfiguration props using  props

---
+++ src/components/SvgConfiguration.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let {
+		svgConfig = $bindable()
+	}: { svgConfig: { bkgHexDark: String; bkgHexLight: String; font: String } } = $props()
+</script>
+
+<section class="flex flex-col gap-y-4 px-8 py-4 text-sm">
+	<article>
+		<p>Bkg dark</p>
+		<input
+			bind:value={svgConfig.bkgHexDark}
+			class="text-blac flex h-6 w-20 items-center justify-center rounded-full p-2 text-black outline-none"
+			style="background-color: {svgConfig.bkgHexDark};"
+			type="text"
+		/>
+	</article>
+
+	<article>
+		<p>Bkg light</p>
+		<input
+			bind:value={svgConfig.bkgHexLight}
+			class="flex h-6 w-20 items-center justify-center rounded-full p-2 text-black outline-none"
+			style="background-color: {svgConfig.bkgHexLight};"
+			type="text"
+		/>
+	</article>
+
+	<article>
+		<p>Font</p>
+		<p>{svgConfig.font}</p>
+	</article>
+
+	<div class="flex w-full items-center justify-between rounded-full bg-red-800">
+		<button>Prev</button>
+		<p>Only icons</p>
+		<button>Post</button>
+	</div>
+</section>
+
+<style>
+	article {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,17 @@
 	import Header from '../components/Header.svelte'
 	import SearchNav from '../components/SearchNav.svelte'
 	import Selector from '../components/Selector.svelte'
+	import SvgConfiguration from '../components/SvgConfiguration.svelte'
+	import SvgProduct from '../components/SvgProduct.svelte'
 	import Swapy from '../components/Swapy.svelte'
 	import { devDependencies } from '../data/devDependencies'
 
 	let selected: (typeof devDependencies)[number]['name'][] = $state([])
+	let svgConfig = $state({
+		bkgHexDark: '#9E66FF',
+		bkgHexLight: '#2777C4',
+		font: 'Roboto'
+	})
 
 	function handleSelected(dependency: (typeof devDependencies)[number]['name']) {
 		selected = !selected.includes(dependency)
@@ -13,17 +20,17 @@
 			: selected.filter((item) => item !== dependency)
 	}
 
-	// $inspect(selected)
+	$inspect(svgConfig)
 </script>
 
 <Header />
 
 <main
-	class="grid grid-cols-[0.3fr_0.5fr_0.2fr] overflow-y-hidden border-y-4 border-y-[#9E66FF]/40 bg-[#2A2D2E] p-4"
+	class="grid grid-cols-[0.2fr_0.6fr_0.2fr] overflow-y-hidden border-y-4 border-y-[#9E66FF]/40 bg-[#2A2D2E] p-4"
 >
 	<Swapy {selected} />
-	<p>Welcome to SvelteKit</p>
-	<p>Welcome to SvelteKit</p>
+	<SvgProduct />
+	<SvgConfiguration bind:svgConfig />
 </main>
 
 <footer class="grid grid-cols-[0.3fr_auto]">
