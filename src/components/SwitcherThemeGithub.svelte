a409f23f271f3b7818739cc5cec4ca3ef69eefc3
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-06 11:56:36 +0100

    Preview icon w/o functionability

--- src/components/SwitcherThemeGithub.svelte
+++ src/components/SwitcherThemeGithub.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import GitHubIcon from '../assets/icons/GitHub.svelte'
+	import PreviewIcon from '../assets/icons/Preview.svelte'
 
 	let { githubTheme = $bindable() } = $props()
+
+	let previewIconColor = $derived(() => (githubTheme === 'dark' ? '#fff' : '#000'))
 </script>
 
-<div class="absolute bottom-0 right-0 flex p-2">
+<div class="absolute bottom-0 right-0 flex items-center justify-center p-2">
+	<button class="px-4 py-2">
+		<PreviewIcon className="size-6" hexColor={previewIconColor()} />
+	</button>
 	<button
 		class={`px-4 py-2 ${githubTheme === 'light' ? 'bg-red-800' : ''}`}
 		onclick={() => (githubTheme = 'light')}
