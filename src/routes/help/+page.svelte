7495f0d56b78a98cc75d06f8b21a888edfa45706
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-08 12:01:23 +0100

    First content of '/help' page

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,1 +1,17 @@
-<h1>help page</h1>
+<section class="flex flex-col items-start justify-start gap-y-4 p-16 py-4">
+	<article>
+		<h1 class="text-2xl font-medium">• How DevSVG works</h1>
+		<ul class="flex list-decimal flex-col gap-y-4 pl-12 pt-4">
+			<li>Select a group of dependencies</li>
+			<li>Configure the properties of the SVG</li>
+			<li>Copy or download the SVG</li>
+			<li>Paste the SVG in your project</li>
+			<li>Included in the README file</li>
+		</ul>
+	</article>
+
+	<article>
+		<h4 class="text-2xl font-medium">• Watch a demo of DevSVG</h4>
+		<p class="flex flex-col gap-y-4 pl-8 pt-4">Demo video</p>
+	</article>
+</section>
