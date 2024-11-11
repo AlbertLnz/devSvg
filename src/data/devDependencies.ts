f34d5c9bb38c8629295dd7dfac7b0bd1160dc69c
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-11 16:56:08 +0100

    Add some JS dependencies

--- src/data/devDependencies.ts
+++ src/data/devDependencies.ts
@@ -1,5 +1,5 @@
 import mistralIcon from '@/data/svgs/python/mistral.svelte'
-import typescriptIcon from '@/data/svgs/typescript.svelte'
+import typescriptIcon from '@/data/svgs/javascript/typescript.svelte'
 import matplotlibIcon from '@/data/svgs/python/matplotlib.svelte'
 import numpyIcon from '@/data/svgs/python/numpy.svelte'
 import pytorchIcon from '@/data/svgs/python/pytorch.svelte'
@@ -17,6 +17,15 @@ import PolarsIcon from '@/data/svgs/python/polars.svelte'
 import FoliumIcon from '@/data/svgs/python/folium.svelte'
 import BokehIcon from '@/data/svgs/python/bokeh.svelte'
 import VizzuIcon from '@/data/svgs/python/vizzu.svelte'
+import MapboxIcon from '@/data/svgs/javascript/mapbox.svelte'
+import ZodIcon from '@/data/svgs/javascript/zod.svelte'
+import TailwindCSSIcon from '@/data/svgs/javascript/tailwindcss.svelte'
+import VitestIcon from '@/data/svgs/javascript/vitest.svelte'
+import ConsoleNinjaIcon from '@/data/svgs/javascript/consoleninja.svelte'
+import ESLintIcon from './svgs/javascript/eslint.svelte'
+import GraphQLIcon from './svgs/javascript/graphql.svelte'
+import NodemonIcon from './svgs/javascript/nodemon.svelte'
+import CloudinaryIcon from './svgs/javascript/cloudinary.svelte'
 
 export const devDependencies = [
 	{
@@ -113,5 +122,50 @@ export const devDependencies = [
 		name: 'Vizzu',
 		language: 'Python',
 		svg: VizzuIcon
+	},
+	{
+		name: 'Mapbox',
+		language: 'JavaScript',
+		svg: MapboxIcon
+	},
+	{
+		name: 'Zod',
+		language: 'JavaScript',
+		svg: ZodIcon
+	},
+	{
+		name: 'TailwindCSS',
+		language: 'JavaScript',
+		svg: TailwindCSSIcon
+	},
+	{
+		name: 'Vitest',
+		language: 'JavaScript',
+		svg: VitestIcon
+	},
+	{
+		name: 'Console Ninja',
+		language: 'JavaScript',
+		svg: ConsoleNinjaIcon
+	},
+	{
+		name: 'ESLint',
+		language: 'JavaScript',
+		svg: ESLintIcon
+	},
+	{
+		name: 'GraphQL',
+		language: 'JavaScript',
+		svg: GraphQLIcon
+	},
+	{
+		name: 'Nodemon',
+		language: 'JavaScript',
+		svg: NodemonIcon
+	},
+	{
+		name: 'Cloudinary',
+		language: 'JavaScript',
+		svg: CloudinaryIcon
 	}
 ]
---
+++ src/data/svgs/javascript/consoleninja.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let {
+		className,
+		width = 30,
+		height = 30
+	}: { className?: string; width?: number; height?: number } = $props()
+</script>
+
+<svg class={className} {width} {height} viewBox="0 0 256 256">
+	<rect
+		width="240"
+		height="240"
+		x="8"
+		y="8"
+		rx="75"
+		ry="100"
+		style="fill:#000;fill-opacity:1;stroke:#c8c8c8;stroke-width:15"
+	/>
+	<rect
+		width="172"
+		height="100"
+		x="42"
+		y="78"
+		rx="42"
+		ry="42"
+		style="display:block;fill:#ffd55c;fill-opacity:1"
+	/>
+	<path stroke="#000" stroke-width="22" d="M142 128h45" /><path
+		stroke="#000"
+		stroke-width="20"
+		d="m74 102 33 33M74 154l33-33"
+	/>
+</svg>
